00b11b365a5194862e55c8585a32bcc5e62856b7
hacefalo <mattianascil2005@gmail.com>
2026-07-15 23:08:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // Serve a capire quale versione del file è davvero online:
 // aprendo la Console del browser deve comparire questa riga.
-console.log("HACE FALÒ 2026 — script.js v4 (hero spaziata)");
+console.log("HACE FALÒ 2026 — script.js v5 (titolo spaziato)");
 
 /* ---------------------------------------------------------
    1) CONFIGURAZIONE — modifica SOLO questo blocco
